ee2d4e39118f6eacc0bae439d48155f07e541101
Daniel Shorten <danielshorten@gmail.com>
2013-04-24 16:59:32 +0300

    Renamed module activator and updated config.xml

--- api/src/main/java/org/openmrs/module/openhmis/workorder/WorkOrderModuleActivator.java
+++ api/src/main/java/org/openmrs/module/openhmis/workorder/WorkOrderModuleActivator.java
@@ -21,7 +21,7 @@ import org.openmrs.module.ModuleActivator;
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
-public class OpenHMISWorkOrderActivator implements ModuleActivator {
+public class WorkOrderModuleActivator implements ModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
 		
