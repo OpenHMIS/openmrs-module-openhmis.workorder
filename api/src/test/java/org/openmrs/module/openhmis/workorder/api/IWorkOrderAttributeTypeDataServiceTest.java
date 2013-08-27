d138533883cac185bc1914c396c77093b9e3d492
Wes Brown <wes@weslandia.org>
2013-08-27 11:48:33 +0300

    Removed copyright from test and xml files.

--- api/src/test/java/org/openmrs/module/openhmis/workorder/api/IWorkOrderAttributeTypeDataServiceTest.java
+++ api/src/test/java/org/openmrs/module/openhmis/workorder/api/IWorkOrderAttributeTypeDataServiceTest.java
@@ -1,19 +1,4 @@
-/*
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
- */
 package org.openmrs.module.openhmis.workorder.api;
-
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,6 +6,8 @@ import org.junit.Test;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.openhmis.workorder.api.model.WorkOrderAttributeType;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+import java.util.List;
 
 public class IWorkOrderAttributeTypeDataServiceTest extends BaseModuleContextSensitiveTest {
 	private IWorkOrderAttributeTypeDataService service;
